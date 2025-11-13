53a28646e0d6a91885fc0aa1a34bc8c93c8f839f
Zida02 <olaty22bisi@gmail.com>
2025-11-13 06:10:06 +0100

    update

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,21 +1,13 @@
-
-
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        
-    } catch (error) {
-        
-    }
-    
-}
+  const { email, password } = req.body;
+  try {
+  } catch (error) {}
+};
 
-
-const registerUser = async (req, res) => { 
-    const { name, email, password } = req.body;
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
